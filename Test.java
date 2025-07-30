cd09724f732520b89a7a61b0f5f5c016da09d0ea
SmdSameer99 <smdsameer99@gmail.com>
2025-07-30 15:17:30 +0530

    update as 30/07

--- Test.java
+++ Test.java
@@ -1,3 +1,12 @@
 public class Test {
     
+    public void printMessage() {
+        System.out.println("This is a test message.");
+    }
+
+    public static void main(String[] args) {
+
+        Test test = new Test();
+        test.printMessage();
+    }
 }
