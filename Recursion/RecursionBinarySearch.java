64e2e5e792e53c543599e0e35722b030197a5ec5
SmdSameer99 <smdsameer99@gmail.com>
2025-08-04 00:03:33 +0530

    binary search using recursion 01/06

--- Recursion/RecursionBinarySearch.java
+++ Recursion/RecursionBinarySearch.java
@@ -18,14 +18,17 @@ static int search(int[] arr, int target, int s, int e) {
 
     int m = s + (e -1) / 2;
 
+    // checking middle element
     if(arr[m] == target){
         return m;
     }
 
+    // checking if target is smaller than middle element
     if(target < arr[m]){
         return search(arr, target, s, m-1);
     }
 
+    // checking if target is greater than middle element
     return search(arr, target, m+1, e);
 }
 }
