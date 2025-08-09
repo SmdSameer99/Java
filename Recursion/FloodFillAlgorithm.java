59697dbb8b4b7fffd45b8292a702c377746ec911
SmdSameer99 <smdsameer99@gmail.com>
2025-08-09 19:23:44 +0530

    Note 09/08

---
+++ Recursion/FloodFillAlgorithm.java
@@ -0,0 +1,15 @@
+package Recursion;
+
+public class FloodFillAlgorithm {
+    
+    public static void main(String[] args) {
+        int[][] image = {
+            {1, 1, 1, 0},
+            {0, 1, 1, 1},
+            {1, 0, 1, 1}
+        };
+
+        int sr = 1, sc = 2, newColor = 2;
+        
+    }
+}
