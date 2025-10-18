8aa52f5c53b0015216ef46c1ec9aa3fd9cf26d7f
SmdSameer99 <smdsameer99@gmail.com>
2025-10-18 18:52:34 +0530

    update 18/10

--- Recursion/RecursionBinarySearch.java
+++ Recursion/RecursionBinarySearch.java
@@ -1,45 +1,30 @@
 package Recursion;
 
 public class RecursionBinarySearch {
-        static long startTime = System.currentTimeMillis();
 
+    public static int binarySearch(int arr[], int target, int start, int end){
+
+        int index = 0;
+        
+        if(start > end){
+            return -1;
+        }
+        int mid = start + (end - start)/2;
+
+        if(arr[mid] == target){
+            return mid;
+        }else if (arr[mid] > target){
+            index = binarySearch(arr, target, start, mid - 1);
+        }else{
+            index = binarySearch(arr, target, mid + 1, end);
+        }
+        return index;
+    }
 
     public static void main(String[] args) {
-    
-    int[] arr = {2, 3, 4, 10, 40, 44, 54, 56, 78, 99};
-
-    //give me larger array with 200 elements
-    int[] arrLarg = new int[200];
-    for (int i = 0; i < arrLarg.length; i++) {
-        arrLarg[i] = i * 2; // fill with even numbers
+        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        int target = 5;
+        int result = binarySearch(arr, target, 0, arr.length - 1);
+        System.out.println("Element found at index: " + result);
     }
-
-     int search = 40;
-     System.out.println(search(arrLarg, search, 0, arrLarg.length - 1));
-
-    // Example usage
-        System.out.println("Time taken: " + (System.currentTimeMillis() - startTime) + " ms");
-}
-
-static int search(int[] arr, int target, int s, int e) {
-     
-    if (s > e) {
-        return -1;
-    }
-
-    int m = s + (e -1) / 2;
-
-    // checking middle element
-    if(arr[m] == target){
-        return m;
-    }
-
-    // checking if target is smaller than middle element
-    if(target < arr[m]){
-        return search(arr, target, s, m-1);
-    }
-
-    // checking if target is greater than middle element
-    return search(arr, target, m+1, e);
-}
 }
